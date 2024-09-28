e96c737db974dd0e8dfa362e465dd75a9d24c9d4
trieungochai <trieungochai.dev@gmail.com>
2024-09-28 14:08:19 +0700

    act-01.01-defining-and-printing

---
+++ 01-variables-and-operators/act-01.01-defining-and-printing/main.go
@@ -0,0 +1,17 @@
+// going to create a medical form for a doctor’s office to capture a patient’s name, age, and whether they have a peanut allergy
+
+package main
+
+import "fmt"
+
+func main() {
+	firstName := "kimchi"
+	lastName := "cukai"
+	age := 60
+	peanutAllergy := true
+
+	fmt.Println(firstName)
+	fmt.Println(lastName)
+	fmt.Println(age)
+	fmt.Println(peanutAllergy)
+}
