07f464cb42303265920852879ac293e308c58d03
trieungochai <trieungochai.dev@gmail.com>
2024-10-06 00:32:58 +0700

    ex.-01.13-getting-a-pointer

---
+++ 01-variables-and-operators/ex.-01.13-getting-a-pointer/main.go
@@ -0,0 +1,23 @@
+// we’ll use each of the methods we can use to get a pointer variable.
+// Then, we’ll print them to the console using fmt.Printf to see what their types and value are.
+
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	var count1 *int
+	count2 := new(int)
+	countTemp := 5
+	count3 := &countTemp
+	t := &time.Time{}
+
+	fmt.Printf("count1: %#v\n", count1)
+	fmt.Printf("count2: %#v\n", count2)
+	fmt.Printf("count3: %#v\n", count3)
+	fmt.Printf("countTemp: %#v\n", countTemp)
+	fmt.Printf("time  : %#v\n", t)
+}
