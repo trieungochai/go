085bad1684120b1dc3c0e87b0a46632eb4522411
trieungochai <trieungochai.dev@gmail.com>
2024-10-20 14:40:46 +0700

    ex.-01.16-constants

---
+++ 01-variables-and-operators/ex.-01.16-constants/main.go
@@ -0,0 +1,62 @@
+// In this exercise, we have a performance problem: our database server is too slow.
+// We are going to create a custom memory cache.
+// We’ll use Go’s map collection type, which will act as the cache.
+// There is a global limit on the number of items that can be in the cache.
+// We’ll use one map to help keep track of the number of items in the cache.
+// We have 2 types of data we need to cache: books and CDs.
+// Both use the ID, so we need a way to separate the two types of items in the shared cache.
+// We need a way to set and get items from the cache.
+// We’re going to set the maximum number of items in the cache.
+// We’ll also use constants to add a prefix to differentiate between books and CDs.
+
+package main
+
+import "fmt"
+
+const GlobalLimit = 100
+const MaxCacheSize int = 10 * GlobalLimit
+
+const (
+	CacheKeyBook = "book_"
+	CacheKeyCD   = "cd_"
+)
+
+var cache map[string]string
+
+func SetBook(isbn string, title string) {
+	key := CacheKeyBook + isbn
+
+	if len(cache)+1 >= MaxCacheSize {
+		return
+	}
+
+	cache[key] = title
+}
+
+func SetCD(sku string, title string) {
+	key := CacheKeyCD + sku
+
+	if len(cache)+1 >= MaxCacheSize {
+		return
+	}
+
+	cache[key] = title
+}
+
+func GetBook(isbn string) string {
+	return cache[CacheKeyBook+isbn]
+}
+
+func GetCD(sku string) string {
+	return cache[CacheKeyCD+sku]
+}
+
+func main() {
+	cache = make(map[string]string)
+
+	SetBook("1234-5678", "Brave New World")
+	SetCD("1234-5678", "Brave New World (Audiobook)")
+
+	fmt.Println("Book :", GetBook("1234-5678"))
+	fmt.Println("CD   :", GetCD("1234-5678"))
+}
