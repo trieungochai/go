df3cc1b7a536e7577fc99adb616d592a0f8a89d6
trieungochai <trieungochai.dev@gmail.com>
2024-12-13 00:45:50 +0700

    ex.-07.01-implementing-an-interface

---
+++ chap-07-interfaces/ex.-07.01-implementing-an-interface/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+type Speaker interface {
+	Speak() string
+}
+
+type person struct {
+	name      string
+	age       int
+	isMarried bool
+}
+
+func main() {
+	p := person{name: "Cailyn", age: 44, isMarried: false}
+	fmt.Println(p.Speak())
+	fmt.Println(p)
+}
+
+func (p person) Speak() string {
+	return "Hi my name is " + p.name
+}
+
+func (p person) String() string {
+	return fmt.Sprintf("%v (%v years old).\nMarried status: %v ", p.name, p.age, p.isMarried)
+}
