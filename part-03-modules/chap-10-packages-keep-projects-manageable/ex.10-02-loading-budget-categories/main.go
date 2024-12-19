072972a4c2aecff238a41d708e09c395b98ee6d5
Hai Trieu Ngoc <trieungochai.dev@gmail.com>
2024-12-19 14:19:53 +0700

    packages: ex.10-02-loading-budget-categories

---
+++ part-03-modules/chap-10-packages-keep-projects-manageable/ex.10-02-loading-budget-categories/main.go
@@ -0,0 +1,24 @@
+// Write a program that will load budget categories into a global map, before the main() function runs.
+// The main() function should then print out the data on the map.
+package main
+
+import "fmt"
+
+var budgetCategories = make(map[int]string)
+
+func init() {
+	fmt.Println("Initializing our budgetCategories")
+	budgetCategories[1] = "Car Insurance"
+	budgetCategories[2] = "Mortgage"
+	budgetCategories[3] = "Electricity"
+	budgetCategories[4] = "Retirement"
+	budgetCategories[5] = "Vacation"
+	budgetCategories[7] = "Grocercies"
+	budgetCategories[8] = "Car Payment"
+}
+
+func main() {
+	for k, v := range budgetCategories {
+		fmt.Printf("key: %d, value: %s\n", k, v)
+	}
+}
